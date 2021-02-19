21a42ea08fba537ad00b362597e7e2353e049f89
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2021-02-19 09:56:17 +0000

    feat: posting data from greenhouse to firebase

---
+++ src/dfGreenhouse.ts
@@ -0,0 +1,42 @@
+import {dbInterface, Candidate} from './candidate';
+import axios, {AxiosResponse} from "axios";
+
+const greenhouseAuth = 'Basic NGMyYTQwN2I4OWRjNjhlNzk2NThiNGMyYjMxNmZmNDMtMzo=';
+const greenhouseUrl = 'https://harvest.greenhouse.io';
+
+
+export default class DfGreenhouse {
+
+  init() {
+    async function getGreenhouseData() {
+      return axios.get(`${greenhouseUrl}/v1/candidates?per_page=500`, {
+        headers: {
+          Authorization: greenhouseAuth
+        }
+      });
+    }
+  
+    function main(start: string): Promise<AxiosResponse<any>> {
+      console.log(`main: ${start}`);
+      return getGreenhouseData();
+    }
+  
+    main('Go!!').then(data => {
+      console.log('data received');
+      const db: dbInterface = {
+        saveCandidate(candidate: Candidate): boolean {
+          console.log(`${candidate.first_name} ${candidate.last_name}`);
+          return true;
+        }
+      };
+  
+      const core = data['data'];
+      for (let dataKey in core) {
+        const candidate = core[dataKey];
+        db.saveCandidate(candidate);
+      }
+      console.log('DONE');
+    }
+  }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 import DfFirebase from "./firebase";
-// import App from "./firebase";
+import DfGreenhouse from './dfGreenhouse';
+
+async function main() {
+    const dfFirebase = new DfFirebase();
+    const dfGreenhouse = new DfGreenhouse();
+    dfFirebase.initialize();
+
+   
+    const dataResult = await dfGreenhouse.init();
+
+    function isPostedtoDf(){
+        for(let el of [dataResult]) {
+            const data = dataResult[el];
+                dfFirebase.post({
+                    data:data
+                });
+            }
+            console.log("done");
+        }
+        isPostedtoDf;
+    }
+    
+    main();
+    
 
 
-function main() {
-    const app = new DfFirebase();
-    app.initialize();
-    app.post({
-        flowers:'strawberry'
-    });
-    console.log("done");
-}
-
-main();
+// dfFirebase.post({
+//     flowers:'strawberry'
+// });
+// console.log("done");
