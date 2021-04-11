99875c23f7e667bb4fc06e85cd99a1adabcbe2c2
BlueMonkey <59574576+Viki-Robles@users.noreply.github.com>
2021-04-11 21:01:36 +0100

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ class DfFirebase {
   db: any;
   initialize() {
     const app = {
-      apiKey: "AIzaSyAf9FDSU6oB1jlyCJHR-1Fbx2elX8-u1hU",
-      authDomain: "fir-test-b1e99.firebaseapp.com",
-      databaseURL: "https://fir-test-b1e99-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "fir-test-b1e99",
-      storageBucket: "fir-test-b1e99.appspot.com",
-      messagingSenderId: "455861716400",
-      appId: "1:455861716400:web:07ffbaa269b9cc53b10275"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
   };
     admin.initializeApp(app);
     this.db = admin.firestore();
